91ee071b5c2a926b6ea5f09552977e52b9ad448b
AdiatsaPutra <santikosetyo77@gmail.com>
2021-02-08 11:41:33 +0700

    add confirmed, recovered, deaths, latUpdates covidData

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -10,30 +10,34 @@ export const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
     }
     return (
         <div className={styles.container}>
-            <Grid container spacing={3} justify="center"> 
+            <Grid container spacing={10} justify="center"> 
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textPrimary" gutterBottom>Positif</Typography>
                         <Typography variant="h5">
                             <CountUp start={0} end={confirmed.value} duration={3.5} separator="," />
                         </Typography>
-                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString}</Typography>
                         <Typography variant="body2">Positif Covid</Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textPrimary" gutterBottom>Sembuh</Typography>
-                        <Typography variant="h5">{recovered.value}</Typography>
-                        <Typography color="textSecondary">REAL DATA</Typography>
+                        <Typography variant="h5">
+                            <CountUp start={0} end={recovered.value} duration={3.5} separator="," />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString}</Typography>
                         <Typography variant="body2">Sembuh Covid</Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textPrimary" gutterBottom>Meninggal</Typography>
-                        <Typography variant="h5">{deaths.value}</Typography>
-                        <Typography color="textSecondary">REAL DATA</Typography>
+                        <Typography variant="h5">
+                            <CountUp start={0} end={deaths.value} duration={3.5} separator="," />
+                        </Typography>
+                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString}</Typography>
                         <Typography variant="body2">Meninggal</Typography>
                     </CardContent>
                 </Grid>
